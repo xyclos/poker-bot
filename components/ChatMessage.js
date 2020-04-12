116eb67ce58bf9087f3c4f129db030587d720a8e
Jake Johnson <jake.johnson@somoglobal>
2020-04-12 18:48:42 -0400

    use functional components

--- components/ChatMessage.js
+++ components/ChatMessage.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react'
 
-class ChatMessage extends Component {
-
-    render() {
-        const { position = 'left', message } = this.props;
-        const isRight = position.toLowerCase() === 'right';
-
-        const align = isRight ? 'text-right' : 'text-left';
-        const justify = isRight ? 'justify-content-end' : 'justify-content-start';
-
-        const messageBoxStyles = {
-            maxWidth: '70%',
-            flexGrow: 0
-        };
-
-        const messageStyles = {
-            fontWeight: 500,
-            lineHeight: 1.4,
-            whiteSpace: 'pre-wrap'
-        };
-
-        return <div className={`w-100 my-1 d-flex ${justify}`}>
+const ChatMessage = props => {
+    const { position = 'left', message } = props
+    const isRight = position.toLowerCase() === 'right'
+    
+    const align = isRight ? 'text-right' : 'text-left'
+    const justify = isRight ? 'justify-content-end' : 'justify-content-start'
+    
+    const messageBoxStyles = {
+        maxWidth: '70%',
+        flexGrow: 0
+    }
+    
+    const messageStyles = {
+        fontWeight: 500,
+        lineHeight: 1.4,
+        whiteSpace: 'pre-wrap'
+    }
+    
+    return (
+        <div className={`w-100 my-1 d-flex ${justify}`}>
             <div className="bg-light rounded border border-gray p-2" style={messageBoxStyles}>
             <span className={`d-block text-secondary ${align}`} style={messageStyles}>
-              {this.props.obscure ? '❓' : message}
+              {props.obscure ? '❓' : message}
             </span>
             </div>
         </div>
-    }
-
+    )
 }
 
-export default ChatMessage;
+export default ChatMessage
